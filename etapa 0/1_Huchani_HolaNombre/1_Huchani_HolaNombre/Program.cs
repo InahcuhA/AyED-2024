9345196fbf6e29e78aacb3acbd5c26ae1722bb56
InahcuhA <167467716+InahcuhA@users.noreply.github.com>
2024-04-18 16:46:56 -0300

    tp1_huchani_HolaNombre

---
+++ etapa 0/1_Huchani_HolaNombre/1_Huchani_HolaNombre/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Huchani_HolaNombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.WriteLine(" como te llamas?");
+
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+            Console.ReadKey();
+   
+        }
+    }
+}

0056cb950aac95312916f19dd7bf78564af50bb4
InahcuhA <167467716+InahcuhA@users.noreply.github.com>
2024-05-03 10:35:08 -0300

    tp1_huchani_LaSumaAnterior

---
+++ etapa1/tp1_huchani_LaSumaAnterior/tp1_albert_huchani/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp1_albert_huchani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*Crear 3 variables numéricas con el valor que tu quieras y
+           en otra variable numérica guardar el valor de la suma de las 3 anteriores. 
+           Mostrar por consola.*/
+
+            int num1 = 8;
+            int num2 = 6;
+            int num3 = 4;
+            int sumadelosnumeros = (num1 + num2 + num3);
+            Console.WriteLine("la suma de los tres numeros es igual a " + sumadelosnumeros );
+
+            Console.ReadKey();
+
+        }
+    }
+}

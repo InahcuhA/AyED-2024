4d910efd12d0aefd49b957b85250358ea436ddea
InahcuhA <167467716+InahcuhA@users.noreply.github.com>
2024-05-03 11:10:42 -0300

    tp3_huchani_DimensionesDeEsfera

---
+++ etapa1/tp3_huchani_DimensionesDeEsfera/tp4_albert_huchani/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp4_albert_huchani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { /*Crea un programa en C# para calcular la superficie y 
+            el volumen de una esfera, dado su radio.
+
+            superficie = 4 * pi * radio al cuadrado
+            volumen = 4 / 3 * pi * radio al cubo*/
+
+
+            Console.WriteLine("ingrese un valor para el radio");
+            double Radio = double.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine("para la superficie calculamos con una operacion 4 * pi * radio al cuadrado");
+            Console.WriteLine("que el resultado es igual a " + (4* Math.PI * (Radio * Radio) ));
+
+            Console.WriteLine("para la volumen calculamos con una operacion 4 / 3 * pi * radio al cubo");
+            Console.WriteLine("que el resultado es igual a " + ((4  * Math.PI) / 3 * (Radio * Radio * Radio)));
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}

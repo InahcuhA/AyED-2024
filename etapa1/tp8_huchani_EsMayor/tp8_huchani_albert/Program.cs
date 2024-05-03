f0bd5d3038cfe55fea6ad2c26fcfcaa51c1b7d58
InahcuhA <167467716+InahcuhA@users.noreply.github.com>
2024-05-03 11:15:25 -0300

    tp8_huchani_EsMayor

---
+++ etapa1/tp8_huchani_EsMayor/tp8_huchani_albert/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp8_huchani_albert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*Escribir un programa que pregunte al usuario su edad y 
+           muestre por pantalla si es mayor de edad o no. */
+            Console.WriteLine("ingrese su edad");
+            int edad = int.Parse(Console.ReadLine());
+
+            if (edad >= 18){
+                Console.WriteLine("usted es mayor de edad");
+            }
+            else
+            { 
+                Console.WriteLine("usted no es mayor de edad");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}

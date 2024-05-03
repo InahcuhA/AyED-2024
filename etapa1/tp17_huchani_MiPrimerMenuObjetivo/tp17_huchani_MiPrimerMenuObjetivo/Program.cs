ffec11c348ace752054cd1db3a73dbf1e8b56b9f
InahcuhA <167467716+InahcuhA@users.noreply.github.com>
2024-05-03 11:43:18 -0300

    tp17_huchani_MiPrimerMenuObjetivo

---
+++ etapa1/tp17_huchani_MiPrimerMenuObjetivo/tp17_huchani_MiPrimerMenuObjetivo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp17_huchani_MiPrimerMenuObjetivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { /*Crear un programa en C# que presente un menú con diversas opciones simples y 
+            permita al usuario seleccionar una de ellas.
+
+            Descripción:
+            Se solicita crear un programa en C# que muestre un menú con al menos tres opciones 
+            simples y permita al usuario seleccionar una de ellas. Cada opción debe realizar una
+            acción diferente, como mostrar un mensaje en pantalla o realizar un cálculo básico.
+
+            Requerimientos:
+            Mostrar un mensaje de bienvenida al programa.
+            Presentar un menú con al menos tres opciones numeradas.
+            Permitir al usuario seleccionar una opción ingresando el número correspondiente.
+            Realizar una acción diferente para cada opción seleccionada.
+            Repetir el proceso hasta que el usuario elija una opción para salir del programa.
+
+
+            Ejemplo de Menú:
+
+            --- Menú ---
+            1. Opción 1
+            2. Opción 2
+            3. Opción 3
+            4. Salir*/
+
+            Console.WriteLine("bienvenido al menu!!!" + "\n" +  "elija una opcion" + "\n" +"1.para realizar una operacion de suma" + "\n" + "2.para decirte cuantos años tienes en este año" + "\n" + "3.para decirte si eres mayor de edad o no" + "\n" + "4.para salir del menu");
+            
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}

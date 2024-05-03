9fd5170dabeb1d9c989dc08d8a60c5d4131e0574
InahcuhA <167467716+InahcuhA@users.noreply.github.com>
2024-05-03 11:32:29 -0300

    tp13_huchani_diezVeces

---
+++ etapa1/tp13_huchani_diezVeces/tp13_huchani_diezVeces/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp13_huchani_diezVeces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*Escribir un programa que pida al usuario una palabra y 
+          la muestre por pantalla 10 veces.*/
+
+            Console.WriteLine("ingrese una palabra");
+            string palabra = Console.ReadLine();
+            int cont = 0;
+            while (cont < 9)
+            {
+                Console.WriteLine(palabra);
+                cont += 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}

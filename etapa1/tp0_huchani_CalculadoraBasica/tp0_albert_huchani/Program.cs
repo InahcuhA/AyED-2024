9e40f18ecc4160bab6f88ab653d876e78a08ef08
InahcuhA <167467716+InahcuhA@users.noreply.github.com>
2024-05-03 10:33:37 -0300

    tp0_huchani_CalculadoraBasica

---
+++ etapa1/tp0_huchani_CalculadoraBasica/tp0_albert_huchani/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp0_albert_huchani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*Escribe un programa en C# que calcule el resultado de sumar,
+           restar, multiplicar y dividir dos números introducidos por el usuario.
+           Además también debería calcular el resto de la división en la 
+           última linea.*/
+
+            Console.WriteLine("introdusca un numero,  para hacer unos operaciones");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("introdusca otro numero");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("en la suma de los dos numeros que pusiste es igual a " + (num1 + num2));
+            Console.WriteLine("en la resta de los dos numeros que pusiste es igual a " + (num1 - num2));
+            Console.WriteLine("en la multiplicacion de los dos numeros que pusiste es igual a " + (num1 * num2));
+            Console.WriteLine("en la division de los dos numeros que pusiste es igual a " + (num1 / num2));
+            Console.WriteLine("en el resto de la division de los dos numeros que pusiste es igual a " + (num1 % num2));
+
+
+
+
+
+
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}

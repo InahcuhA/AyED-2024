ca90a2698c6e2fd3ba7aaf0c6a2faa1ec64f0f90
InahcuhA <167467716+InahcuhA@users.noreply.github.com>
2024-05-03 11:11:48 -0300

    tp4_huchani_CentigradosKelvinFarenheit

---
+++ etapa1/tp4_huchani_CentigradosKelvinFarenheit/tp4real_huchani_albert/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp4real_huchani_albert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*La escala Celsius es centígrada, 100 divisiones separan el punto de congelación del
+           punto de ebullición del agua. En la escala Fahrenheit de los anglosajones, 
+           estos dos puntos están separados por 180 grados. 
+           La escala de Kelvin es una escala absoluta utilizada en ciencias.
+           Cree un programa en C# para convertir de grados centígrados a Kelvin y Fahrenheit. 
+           Solicite al usuario la cantidad de grados centígrados para convertirlos usando 
+           las siguientes tablas de conversión:
+            
+           kelvin = celsius + 273
+           fahrenheit = celsius x 18 / 10 + 32 */
+
+            Console.WriteLine("ingrese un valor para grados centigrados");
+            float celsius = float.Parse(Console.ReadLine());
+            Console.WriteLine(" En kelvin es igual a " + (celsius + 273));
+            Console.WriteLine(" En fahrenheit es igual a " + (celsius * 18 / 10 + 32));
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}

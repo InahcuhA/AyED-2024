2d351770a360ddca606374589765440c37439f21
InahcuhA <167467716+InahcuhA@users.noreply.github.com>
2024-05-03 11:31:00 -0300

    tp12_huchani_CobrarBeca

---
+++ etapa1/tp12_huchani_CobrarBeca/tp12_huchani_CobrarBeca/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp12_huchani_CobrarBeca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*Para poder cobrar la beca JuanPedro se debe ser mayor de 19 años y 
+           tener unos ingresos iguales o superiores a $100.000 mensuales. 
+           Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales 
+           y muestre por pantalla si el usuario puede cobrar o no.*/
+
+            Console.WriteLine("igrese su edad");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese sus ingrsos mesuales");
+            int ingre = int.Parse(Console.ReadLine());
+            
+
+            if((edad > 19) && (ingre >= 100000))
+            {
+                Console.WriteLine("puede cobrar la beca");
+
+            }
+            else
+            {
+                Console.WriteLine("no puede cobrar la beca");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

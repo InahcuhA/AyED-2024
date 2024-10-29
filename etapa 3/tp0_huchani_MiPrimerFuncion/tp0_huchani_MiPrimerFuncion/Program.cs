f7ff13f87fea46e42ca8caa851160d40dd61ea80
InahcuhA <167467716+InahcuhA@users.noreply.github.com>
2024-10-29 16:11:27 -0300

    tp0_huchani_MiPrimerFuncion

---
+++ etapa 3/tp0_huchani_MiPrimerFuncion/tp0_huchani_MiPrimerFuncion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp0_huchani_MiPrimerFuncion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escribir una función a la que se le pase una cadena <nombre> y 
+            //muestre por pantalla el saludo ¡hola <nombre>!
+
+            saludo("capi");
+            Console.ReadKey();
+        }
+        static void saludo(string nombre)
+        {
+            Console.WriteLine("hola " + nombre);
+        }
+    }
+}

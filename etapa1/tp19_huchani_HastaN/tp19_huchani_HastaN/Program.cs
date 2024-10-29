64cf4485b0636b62d943b49499e23db5acf7acd9
InahcuhA <167467716+InahcuhA@users.noreply.github.com>
2024-10-29 14:40:02 -0300

    tp19_huchani_HastaN

---
+++ etapa1/tp19_huchani_HastaN/tp19_huchani_HastaN/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp19_huchani_HastaN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*Recorre los números del 1 al N. (N lo tiene que ingresar el usuario).
+           Muestra solamente los números pares.*/
+
+            Console.WriteLine("ingrese un numero para hasta que numero cuente, el programa ");
+            int N = int.Parse(Console.ReadLine());
+            int N2 = N;
+            int cont = 1;
+
+            while (cont < N)
+            {
+                if (cont % 2 == 0)
+                {
+                    Console.WriteLine(cont);
+                }
+                cont += 1;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

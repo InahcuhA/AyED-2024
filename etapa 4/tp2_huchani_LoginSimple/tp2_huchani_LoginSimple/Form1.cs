3990f0f3cf460e3f079658ad588f611cfa7d2760
InahcuhA <167467716+InahcuhA@users.noreply.github.com>
2024-10-29 16:23:45 -0300

    tp2_huchani_LoginSimple

---
+++ etapa 4/tp2_huchani_LoginSimple/tp2_huchani_LoginSimple/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tp2_huchani_LoginSimple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string usuario = textBox1.Text;
+            string password = textBox2.Text;
+
+            if ((usuario == "Admin") && (password == "Admin12345"))
+            {
+                MessageBox.Show("Autorizado a ingresar");
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrecta");
+            }
+            
+        }
+    }
+}
